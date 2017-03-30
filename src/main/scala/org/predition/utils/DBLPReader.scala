3eb165927a70732f4560c0311b61bf681c645e45
mohammedi <mohammedi.haroun@gamil.com>
2017-03-30 21:23:22 +0200

    *Change the read method name to extractCoAuthroRelation *Add Documentation

--- src/main/scala/org/predition/utils/DBLPReader.scala
+++ src/main/scala/org/predition/utils/DBLPReader.scala
@@ -1,74 +1,64 @@
 package org.predition.utils
 
 import java.nio.file.{Files, Paths}
-
 import org.apache.spark.sql.{Dataset, SparkSession}
-
 import scala.collection.mutable
 
 /**
-  * Created by mohammedi on 3/29/17.
-  * contient des méthode qui permet de lire les données des fichiers dblp et 
-  * d'extraire les informations relatives à la procedure de prediction, ces
-  * information represente pour le moment la relation co author
+  * Contient des méthodes qui permet de lire les données des fichiers dblp et d'extraire les informations relatives à la
+  * procedure de prediction
+  *
+  * @author mohammedi
   */
 
 object DBLPReader {
-
-  def main(args: Array[String]): Unit = {
-    //Logger.getLogger("org").setLevel(Level.OFF)
-    //Logger.getLogger("akka").setLevel(Level.OFF)
-    val spark = SparkSession
-      .builder()
-      .master("local[*]")
-      .appName("Link Prediction")
-      .getOrCreate()
-
-
-    read(spark, "/home/mohammedi/IdeaProjects/PFEWeb/sparkApplication/input/dblp/data.txt")
-  }
-
-  def read(spark: SparkSession, path: String): Dataset[(Int, Couple)] = {
-    import spark.implicits._
-    if (Files.notExists(Paths.get("years"))) prepareData(spark, path)
+  /**
+    * Extraire les liens de la relation Co-Author
+    *
+    * @example '''(2016, Hamza Labbaci, Youcef Aklouf)''' represete une relation co author entre '''Hamza Labbaci''' et
+    *          '''Youcef AKlouf''' durant l'année '''2016'''
+    * @note cette méthode est creé de tels façon pour ne pas refaire le même travail a chaque fois, c'est pour ça elle
+    *       sauvgarde les relation créés la première fois dans un dossier '''years''' pour les lire dans les prochaines
+    *       eventuelles invocations
+    *       SVP de ne pas supprimer ce dossier pour qu'on puisse optimizer le temps d'exection pour vous
+    * @param spark la session courate du spark cette session est utilisé pour manipulé les `Dataset`
+    * @param path  le lien du fichier dblp téléchargé de [[https://aminer.org/citation]]
+    * @return liens de la relation Co-Author sous la form (année, Couple(nom du 1er auteur, nom du 2eme auteur))
+    * @author mohammedi
+    */
+  def extractCoAuthorRelations(spark: SparkSession, path: String): Dataset[(Int, Couple)] = {
+    if (Files.notExists(Paths.get("years"))) {
+      def notAlone(authors: String) = authors.substring(2).split(',').filterNot(_.matches("[ ]*")).length > 1
+      val content = spark.read.textFile(path)
+      val authorsWithYears = content
+        .filter(line => line.startsWith("#t") || (line.startsWith("#@") && notAlone(line)))
+        .filter(!_.isEmpty())
+        .mapPartitions { iterator =>
+          val result = mutable.MutableList[(Int, Couple)]()
+          while (iterator.hasNext) {
+            var line = iterator.next()
+            var authors = ""
+            while (iterator.hasNext && line.startsWith("#@")) {
+              authors = line.substring(2)
+              line = iterator.next()
+            }
+            if (iterator.hasNext) {
+              if (line.startsWith("#t")) {
+                val year = line.substring(2).trim.toInt
+                val authorsList = authors.split(',').filterNot(_.matches("[ ]*"))
+                for (index1 <- 0 until (authorsList.length - 1); index2 <- index1 + 1 until authorsList.length) {
+                  val c = (year, Couple(authorsList(index1), authorsList(index2)))
+                  result += c
+                }
+              }
+            }
+          }
+          result.toIterator
+        }
+      authorsWithYears.write.parquet("years")
+      authorsWithYears
+    }
     else spark.read.parquet("years").as[(Int, Couple)]
   }
 
-  def prepareData(spark: SparkSession, path: String): Dataset[(Int, Couple)] = {
-    import spark.implicits._
-
-    val map: mutable.Map[Int, mutable.MutableList[Couple]] = mutable.Map()
-
-    def notAlone(authors: String) = authors.substring(2).split(',').filterNot(_.matches("[ ]*")).length > 1
-
-    val content = spark.read.textFile(path)
-
-    val authorsWithYears = content
-    .filter(line => line.startsWith("#t") || (line.startsWith("#@") && notAlone(line)))
-    .filter(!_.isEmpty())
-    .mapPartitions { iterator =>
-      val result = mutable.MutableList[(Int, Couple)]()
-      while (iterator.hasNext) {
-        var line = iterator.next()
-        var authors = ""
-        while (iterator.hasNext && line.startsWith("#@")) {
-          authors = line.substring(2)
-          line = iterator.next()
-        }
-        if (iterator.hasNext) {
-          if (line.startsWith("#t")) {
-            val year = line.substring(2).trim.toInt
-            val authorsList = authors.split(',').filterNot(_.matches("[ ]*"))
-            for (index1 <- 0 until (authorsList.length - 1); index2 <- index1 + 1 until authorsList.length) {
-              val c = (year, Couple(authorsList(index1), authorsList(index2)))
-              result += c
-            }
-          }
-        }
-      }
-      result.toIterator
-    }
-    authorsWithYears.write.parquet("years")
-    authorsWithYears
-  }
 }
